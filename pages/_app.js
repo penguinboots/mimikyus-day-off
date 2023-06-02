9ff58f9013a8a8d0cf7385d6d798cd392a82730e
DASitby <davidsooley551@gmail.com>
2023-06-02 23:54:49 +0000

    add userprovider component

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/globals.scss';
+import '../styles/Home.scss';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
+export default function App({ Component, pageProps }) {
+  return;
+  <UserProvider>
+    <Component {...pageProps} />;
+  </UserProvider>;
+}
