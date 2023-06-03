a6329e986df476c81465e014a7d103dc24831d0d
Joel Hagan <joel.hagan.8@gmail.com>
2023-06-03 15:58:18 -0600

    implement TypeLabel component for PokeInfo

--- components/home/Dashboard/TypeLabel.jsx
+++ components/home/Dashboard/TypeLabel.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React from 'react';
 
-export default function TypeLabel() {
+export default function TypeLabel({ type }) {
+  
+  const getTypeColor = (type) => {
+    switch (type) {
+      case 'normal':
+        return '#A8A77A';
+      case 'fire':
+        return '#EE8130';
+      case 'water':
+        return '#6390F0';
+      case 'electric':
+        return '#F7D02C';
+      case 'grass':
+        return '#7AC74C';
+      case 'ice':
+        return '#96D9D6';
+      case 'fighting':
+        return '#C22E28';
+      case 'poison':
+        return '#A33EA1';
+      case 'ground':
+        return '#E2BF65';
+      case 'flying':
+        return '#A98FF3';
+      case 'psychic':
+        return '#F95587';
+      case 'bug':
+        return '#A6B91A';
+      case 'rock':
+        return '#B6A136';
+      case 'ghost':
+        return '#735797';
+      case 'dragon':
+        return '#6F35FC';
+      case 'dark':
+        return '#705746';
+      case 'steel':
+        return '#B7B7CE';
+      case 'fairy':
+        return '#D685AD';
+      default:
+        return '#000000';
+    }
+  };
+
+  const typeColor = getTypeColor(type);
+
   return (
-    <div>TypeLabel</div>
-  )
+    <span
+      style={{
+        backgroundColor: typeColor,
+        color: '#FFFFFF',
+        padding: '0.25rem 0.5rem',
+        borderRadius: '0.25rem',
+        marginRight: '0.5rem',
+        textTransform: 'capitalize',
+        fontSize: '0.75rem',
+      }}
+    >
+      {type}
+    </span>
+  );
 }
