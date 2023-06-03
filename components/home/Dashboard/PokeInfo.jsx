c2ad0eb78b167652877190e80b1f68eed2d7b08e
Joel Hagan <joel.hagan.8@gmail.com>
2023-06-03 15:58:57 -0600

    pass in TypeLabel to PokeInfo component

--- components/home/Dashboard/PokeInfo.jsx
+++ components/home/Dashboard/PokeInfo.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import TypeLabel from './TypeLabel';
 
 export default function PokeInfo() {
   return (
-    <div className="poke-info-card">PokeInfo</div>
+    <div className="poke-info-card">
+    PokeInfo
+    <TypeLabel type="ghost" />
+    <TypeLabel type="fairy" />
+  </div>
   )
 }
