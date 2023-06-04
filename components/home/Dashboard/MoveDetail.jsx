8eb18fff76274dc3132e1fa2facd99120530bb9f
Joel Hagan <joel.hagan.8@gmail.com>
2023-06-04 16:38:45 -0600

    refactor MoveDetail so it uses deliberate props

--- components/home/Dashboard/MoveDetail.jsx
+++ components/home/Dashboard/MoveDetail.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function MoveDetail({ moveName, power, description }) {
+export default function MoveDetail(props) {
+
+  const { moveName, power, description } = props;
+
+  MoveDetail.propTypes = {
+    moveName: PropTypes.string,
+    power: PropTypes.number,
+    description: PropTypes.string,
+  };
+
   return (
     <div className="move-item">
       <h3>{moveName}</h3>
