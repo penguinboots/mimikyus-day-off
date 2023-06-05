f73b90eb05d890150ec71b576d69cd1216fa3914
penguinboots <sabrina.ynw@gmail.com>
2023-06-05 10:24:29 -0700

    build Achievements menu

---
+++ components/common/AchievementsMenu.jsx
@@ -0,0 +1,10 @@
+export default function AchievementsMenu() {
+  return (
+    <div className="achievement-container">
+      <h2>Achievements</h2>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
