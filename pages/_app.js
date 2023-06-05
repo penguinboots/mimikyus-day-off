746da341ed639d9239ea50a500a5fe1bf8421158
penguinboots <sabrina.ynw@gmail.com>
2023-06-04 17:56:05 -0700

    rename stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/globals.scss';
-import '../styles/Home.scss';
+import '../styles/App.scss';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function App({ Component, pageProps }) {
