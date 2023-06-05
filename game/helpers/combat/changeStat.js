33cec5b287a50354a6a2a96baabcef970b3d1483
DASitby <davidsooley551@gmail.com>
2023-06-05 21:07:09 +0000

    refactor changeStat to output to a variable and append to results object

--- game/helpers/combat/changeStat.js
+++ game/helpers/combat/changeStat.js
@@ -1,5 +1,16 @@
 function changeStat(target, stat, stages) {
-  target.statChanges[stat] += stages
+  //CHANGE THIS TO OUTPUT INSTEAD
+  let statChanges = {
+    "hp": 0,
+    "attack": 0,
+    "defense": 0,
+    "special-attack": 0,
+    "special-defense": 0,
+    "speed": 0,
+    "target": target
+  }
+  statChanges[stat] += stages
+  return statChanges
 }
 
 module.exports = { changeStat }
--- game/helpers/combat/doMove.js
+++ game/helpers/combat/doMove.js
@@ -19,34 +19,35 @@ function doMove(move, user, target) {
   let targetMoveStat = target.stats[targetStat]
   let damage = 0;
   let heal = 0;
+  let statchanges = {}
   //check user for stat changes and apply them to the stat
-  // if (user.statChanges[userStat] > 0) {
-  //   console.log("Multiplier: ", (user.statChanges[userStat] + 2) / 2)
-  //   userMoveStat *= (user.statChanges[userStat] + 2) / 2
-  // } else if (user.statChanges[userStat] < 0) {
-  //   console.log("Multiplier: ", 2 / (user.statChanges[userStat] + 2))
-  //   userMoveStat *= 2 / (user.statChanges[userStat] + 2)
-  // }
+  if (user.statChanges[userStat] > 0) {
+    console.log("Multiplier: ", (user.statChanges[userStat] + 2) / 2)
+    userMoveStat *= (user.statChanges[userStat] + 2) / 2
+  } else if (user.statChanges[userStat] < 0) {
+    console.log("Multiplier: ", 2 / (user.statChanges[userStat] + 2))
+    userMoveStat *= 2 / (user.statChanges[userStat] + 2)
+  }
   //check user for stat changes and apply them to the stat
-  // if (user.statChanges[userStat] > 0) {
-  //   console.log("Multiplier: ", (user.statChanges[userStat] + 2) / 2)
-  //   userMoveStat *= (user.statChanges[userStat] + 2) / 2
-  // } else if (user.statChanges[userStat] < 0) {
-  //   console.log("Multiplier: ", 2 / (user.statChanges[userStat] + 2))
-  //   userMoveStat *= 2 / (user.statChanges[userStat] + 2)
-  // }
+  if (user.statChanges[userStat] > 0) {
+    console.log("Multiplier: ", (user.statChanges[userStat] + 2) / 2)
+    userMoveStat *= (user.statChanges[userStat] + 2) / 2
+  } else if (user.statChanges[userStat] < 0) {
+    console.log("Multiplier: ", 2 / (user.statChanges[userStat] + 2))
+    userMoveStat *= 2 / (user.statChanges[userStat] + 2)
+  }
   //check move category and perform appropriate actions
   if (move.meta.category === "damage") {
-     damage = damageCalc(move, userMoveStat, targetMoveStat, user.types, target.types)
+    damage = damageCalc(move, userMoveStat, targetMoveStat, user.types, target.types)
   } else if (move.meta.category === "damage+lower") {
-     damage = damageCalc(move, userMoveStat, targetMoveStat, user.types, target.types)
-    changeStat(target, target.statChanges[move.stat_changes.stat], move.stat_changes.change)
+    damage = damageCalc(move, userMoveStat, targetMoveStat, user.types, target.types)
+    statChanges = changeStat(target, target.statChanges[move.stat_changes.stat], move.stat_changes.change)
   } else if (move.meta.category === "damage+raise") {
-     damage = damageCalc(move, userMoveStat, targetMoveStat, user.types, target.types)
-    changeStat(user, user.statChanges[move.stat_changes.stat], move.stat_changes.change)
+    damage = damageCalc(move, userMoveStat, targetMoveStat, user.types, target.types)
+    statChanges = changeStat(user, user.statChanges[move.stat_changes.stat], move.stat_changes.change)
   } else if (move.meta.category === "damage+heal") {
-     damage = damageCalc(move, userMoveStat, targetMoveStat, user.types, target.types)
-     heal = drainCalc(damage, move.meta.drain)
+    damage = damageCalc(move, userMoveStat, targetMoveStat, user.types, target.types)
+    heal = drainCalc(damage, move.meta.drain)
   } else if (move.meta.category === "net-good-stats") {
     let changeStatOf = {}
     if (move.target === 'user') {
@@ -54,18 +55,24 @@ function doMove(move, user, target) {
     } else if (move.target === 'selected-pokemon') {
       changeStatOf = target
     }
-    changeStat(changestatOf, changeStatOf.statChanges[move.stat_changes.stat], move.stat_changes.change)
+    statChanges = changeStat(changestatOf, changeStatOf.statChanges[move.stat_changes.stat], move.stat_changes.change)
   } else if (move.meta.category === "unique") {
     if (move.name === "splash") {
        damage = 0
     }
   }
-  const results = {}
-  if (damage) {
+  const results = {
+    damage: null, 
+    heal: null, 
+    statchanges: null
+  }
+  if (damage !== 0) {
     results.damage = damage * 0.5;
   }
-  if (heal) {
-    results.heal = heal
+  if (heal !== 0) {
+    results.heal = heal}
+  if (statChanges.target) {
+    results.statChanges = statChanges
   }
   return results
 }
