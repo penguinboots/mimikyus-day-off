0bd9ee0ac7202a51efd63a85ce9881934d2667da
penguinboots <sabrina.ynw@gmail.com>
2023-06-05 10:12:34 -0700

    implement mode switching from dashboard

--- components/home/Dashboard/Dashboard.jsx
+++ components/home/Dashboard/Dashboard.jsx
@@ -2,13 +2,14 @@ import CharSection from "./CharSection";
 import Nav from "../../common/Nav";
 import EnterGame from "./EnterGame";
 
-export default function Dashboard() {
+export default function Dashboard(props) {
+  const { setMode } = props;
   return (
     <div className="dashboard">
       <Nav />
       <div className="dash-body">
         <CharSection />
-        <EnterGame />
+        <EnterGame setMode={setMode} />
       </div>
     </div>
   );
