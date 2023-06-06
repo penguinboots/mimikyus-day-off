10abd436c96aeebca241ee4a345770a28f4e5694
DASitby <davidsooley551@gmail.com>
2023-06-06 07:44:48 +0000

    add achievements to getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { useState, useEffect, useRef } from 'react';
 // Auth0
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { getUserAchievements, findCurrentUser, findUserCharacters } from "../prisma/script";
+import achievements from '../game/data/achievements.json'
 // Components
 import Landing from '@/components/home/Landing';
 import Dashboard from '@/components/home/Dashboard/Dashboard';
@@ -18,36 +18,56 @@ export async function getStaticProps() {
   const prisma = new PrismaClient();
   let db_user = null
   let db_character = null
+  let db_achievements = []
   db_user = await prisma.user.findUnique({
     where: { auth0Sub: 'auth0sub123' },
   });
   if (db_user) {
-  db_character = await prisma.character.findFirst({
-    where: { userId: db_user.id }
-  })} else {
-  db_user = await prisma.user.create({
-      // data for the new user entered here
-      data: {
-        email: "example@example.com",
-        auth0Sub: "auth0sub123",
-        password: "password123",
-        name: "John Doe",
-      }
-    });
-    db_character = await prisma.character.create({
-      data:{
-        move_1: "Move 1",
-        move_2: "Move 2",
-        move_3: "Move 3",
-        move_4: "Move 4",
-        userId: db_user.id
-      }
+    db_character = await prisma.character.findFirst({
+      where: { userId: db_user.id }
     })
+    db_achievements = await prisma.achievement.findMany({
+        where: { userId: db_user.id }
+      })
+    } else {
+      db_user = await prisma.user.create({
+        // data for the new user entered here
+        data: {
+          email: "example@example.com",
+          auth0Sub: "auth0sub123",
+          password: "password123",
+          name: "John Doe",
+        }
+      });
+      db_character = await prisma.character.create({
+        data:{
+          move_1: "Move 1",
+          move_2: "Move 2",
+          move_3: "Move 3",
+          move_4: "Move 4",
+          userId: db_user.id
+        }
+      })
+      const achievementArray = Object.values(achievements)
+      console.log(achievementArray)
+      for (let i = 0; i < achievementArray.length; i++) {
+        let achievement = achievementArray[i]
+        const db_achievement = await prisma.achievement.create({
+          data:{
+            name: achievement.name,
+            collected: achievement.collected,
+            date_get: null,
+            userId: db_user.id,
+          },
+        })
+        db_achievements.push(db_achievement)
+      };
   }
   return {
     props: {
       db_user,
       db_character,
+      db_achievements
     },
   };
 }
@@ -55,6 +75,7 @@ export async function getStaticProps() {
 export default function Home({
   db_user,
   db_character,
+  db_achievements,
 }) {
   // Authentication
   const { user, error, isLoading } = useUser();
@@ -76,7 +97,7 @@ export default function Home({
   }, [user]);
   console.log("DB_USER INFO: ", db_user);
   console.log("DB_Character:", db_character)
-
+  console.log("DB_achievements?", db_achievements)
   return (
     <div className="app-wrapper">
       <div className="view-wrapper">
