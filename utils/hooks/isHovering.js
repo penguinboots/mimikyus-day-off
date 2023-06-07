c6ca6f32cd387947fdecdc23afab9600f36511f6
penguinboots <sabrina.ynw@gmail.com>
2023-06-06 21:13:52 -0700

    refactor all move hover behaviour

--- utils/hooks/isHovering.js
+++ utils/hooks/isHovering.js
@@ -2,27 +2,47 @@ import { useState } from "react";
 
 export default function useIsHovering() {
   // Hover menu behaviour for movesets
-  // Dashboard & Play menu
+  // Implemented in MoveItem
   const [isHovering, setIsHovering] = useState({
-    move1: false,
-    move2: false,
-    move3: false,
-    move4: false,
+    moveCard: {
+      move1: false,
+      move2: false,
+      move3: false,
+      move4: false,
+    },
+    game: {
+      move1: false,
+      move2: false,
+      move3: false,
+      move4: false,
+    },
+    moveEdit: {
+      move1: false,
+      move2: false,
+      move3: false,
+      move4: false,
+    }
   });
 
-  const handleMouseOver = (id) => {
+  const handleMouseOver = (id, loc) => {
     setIsHovering((prev) => ({
       ...prev,
-      [id]: true,
-    }));
+      [loc]: {
+        ...prev[loc],
+        [id]: true
+      }
+    }))
   };
 
-  const handleMouseOut = (id) => {
+  const handleMouseOut = (id, loc) => {
     setIsHovering((prev) => ({
       ...prev,
-      [id]: false,
-    }));
+      [loc]: {
+        ...prev[loc],
+        [id]: false
+      }
+    }))
   };
-  
+
   return { isHovering, setIsHovering, handleMouseOver, handleMouseOut };
 }
