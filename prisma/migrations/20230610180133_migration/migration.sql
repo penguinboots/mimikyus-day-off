ba7f416d82a517d65de001df06a0679121e1f958
DASitby <davidsooley551@gmail.com>
2023-06-07 07:29:16 +0000

    condense migrations into one

--- prisma/migrations/20230610180133_migration/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Move" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "date_get" DATETIME,
-    "name" TEXT NOT NULL,
-    "collected" BOOLEAN NOT NULL
-);
