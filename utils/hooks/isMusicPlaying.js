7b30358bd68b6fd62507e69decebd29c4e23a164
Joel Hagan <joel.hagan.8@gmail.com>
2023-06-07 14:40:24 -0600

    update returns on useIsMusicPlaying

--- utils/hooks/isMusicPlaying.js
+++ utils/hooks/isMusicPlaying.js
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useState, useRef } from 'react';
 
-export default function useIsMusicPlaying(audioRef, originalVolumeRef) {
+export default function useIsMusicPlaying(audioRef) {
   const [isMusicPlaying, setMusicPlaying] = useState(false);
+  const originalVolumeRef = useRef(0.05);
 
   const handleMusicToggle = () => {
     setMusicPlaying(prev => !prev);
     audioRef.current.volume = isMusicPlaying ? originalVolumeRef.current : 0.05;
   };
 
-  return { isMusicPlaying, setMusicPlaying, handleMusicToggle, audioRef, originalVolumeRef };
+  return { isMusicPlaying, handleMusicToggle };
 }
