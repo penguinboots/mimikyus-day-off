805911a7e269fd7985c33d4d602cd446746a1ead
Joel Hagan <joel.hagan.8@gmail.com>
2023-06-08 16:23:18 -0600

    create functions for prisma

--- prisma/script.ts
+++ prisma/script.ts
@@ -1,48 +1,95 @@
-// import { PrismaClient } from '@prisma/client'
 const { PrismaClient } = require("@prisma/client")
 
 const prisma = new PrismaClient()
 
+// pull user and their achievements
+async function getUserAchievements(userId) {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    include: {
+      achievements: {
+        include: {
+          achievement: true
+        }
+      }
+    }
+  });
+
+  // throw error if user is not found
+  if (!user) {
+    throw new Error(`User with ID ${userId} not found.`);
+  }
+
+  // map over this users achievements
+  const achievements = user.achievements.map((achieved) => ({
+    id: achieved.achievementId,
+    date_get: achieved.date_get,
+    collected: achieved.collected,
+    ...achieved.achievement
+  }));
+
+  return achievements;
+}
+
 async function main() {
-
-  // baseline creation of a user
-  const user = await prisma.user.create(
-    {
+  try {
+    // create a user with an achievement
+    const user = await prisma.user.create({
+      // data for the new user entered here
       data: {
-        email: 'Bob',
-        password: 'POWER 1',
-        name: 'POWER 2',
-      },
-    }
-  )
-  console.log(user)
-
-  //baseline creation of a character once user has been created
-  const character = await prisma.character.create(
-    {
-      data: {
-        user: {
-          connect: {
-            id: 1
-          }
+        email: "example@example.com",
+        auth0Sub: "auth0sub123",
+        password: "password123",
+        name: "John Doe",
+        // data for the new user's achievements
+        achievements: {
+          create: [
+            {
+              date_get: new Date(),
+              achievement: {
+                create: {
+                  achievement_name: "First Achievement",
+                  achievement_image: "achievement_image_url",
+                  achievement_description: "Achievement description"
+                }
+              },
+              collected: false
+            }
+          ]
         },
-        species: 'Fairy',
-        move_1: 'POWER 1',
-        move_2: 'POWER 2',
-        move_3: 'POWER 3',
-        move_4: 'POWER 4',
-        level: 5,
-        hp_stat: 100,
-        attack_stat: 30,
-        defense_stat: 5,
-        spatk_stat: 2,
-        spdef_stat: 0,
-        speed_stat: 0
-      },
-    }
-  )
-  console.log(character)
+        // data for new user's characters
+        Character: {
+          create: {
+            species: "Species",
+            move_1: "Move 1",
+            move_2: "Move 2",
+            move_3: "Move 3",
+            move_4: "Move 4",
+            level: 50,
+            hp_stat: 100,
+            attack_stat: 80,
+            defense_stat: 70,
+            spatk_stat: 90,
+            spdef_stat: 80,
+            speed_stat: 100
+          }
+        }
+      }
+    });
 
+    // show the newly created user
+    console.log("Created user:", user);
+
+    const userId = user.id;
+    const achievements = await getUserAchievements(userId);
+
+    // show us the achievements for that user
+    console.log("User achievements:", achievements);
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 main()
@@ -50,7 +97,12 @@ main()
     await prisma.$disconnect()
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
+      console.error(e)
+      await prisma.$disconnect()
+      process.exit(1)
   })
+
+module.exports = {
+  main,
+  getUserAchievements
+};
