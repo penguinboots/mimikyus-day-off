778776ded3e5f86dd7919765bbfa8df20ae1dc33
penguinboots <sabrina.ynw@gmail.com>
2023-06-08 15:07:55 -0700

    add gameState context

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,14 @@ import React from 'react';
 import '../styles/globals.scss';
 import '../styles/App.scss';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { GameStateProvider } from '@/utils/context/GameStateContext';
 
 export default function App({ Component, pageProps }) {
-  return (<UserProvider>
-    <Component {...pageProps} />
-  </UserProvider>);
+  return (
+    <UserProvider>
+      <GameStateProvider>
+        <Component {...pageProps} />
+      </GameStateProvider>
+    </UserProvider>
+  );
 }
