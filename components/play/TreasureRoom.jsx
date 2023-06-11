c04c18d61c782dda12fb14e7bc52db4a0b818d73
DASitby <davidsooley551@gmail.com>
2023-06-11 11:11:55 -0230

    add some buttons to test database updates

--- components/play/TreasureRoom.jsx
+++ components/play/TreasureRoom.jsx
@@ -1,10 +1,22 @@
-import MoveItem from "../common/MoveItem";
-import { useGameState } from "../../utils/context/GameStateContext";
-
+import { learnMove } from "@/game/helpers/api/learnMove";
+import { acquireAchievement } from "@/game/helpers/api/acquireAchievement"
 export default function Room(props) {
+  const { db_user } = props
   return (
     <div className="treasure-room">
       <div className="treasure-floor">
+        <button onClick={acquireAchievement}>
+          'Click to Update Achievement'
+        </button>
+        <br></br>
+        <button onClick={()=> {learnMove(db_user, 'play-rough')}}>
+          'Click to Learn Play Rough'
+        </button>
+        <br></br>
+        <button onClick={()=> {learnMove(db_user, 'shadow-sneak')}}>
+          'Click to Learn Shadow Sneak'
+        </button>
+        <br></br>
         <div>THIS IS A TREASURE CHEST</div>
       </div>
       <button onClick={props.nextRoom}>NEXT ROOM</button>
