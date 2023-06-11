b4c887b7456cea772fecb8a2a7eb8f5e3b62dd25
DASitby <davidsooley551@gmail.com>
2023-06-11 11:09:26 -0230

    comment out fetch code, continue prop drill

--- components/home/Dashboard/Dashboard.jsx
+++ components/home/Dashboard/Dashboard.jsx
@@ -4,33 +4,33 @@ import EnterGame from "./EnterGame";
 import { useEffect, useState } from "react";
 
 export default function Dashboard(props) {
-  const { mode, setMode, isMusicPlaying, handleMusicToggle } = props;
-  const userId = 1;
-  const achievementName = "something shiny";
+  let { mode, setMode, isMusicPlaying, handleMusicToggle, db_user, db_character, db_moves, db_achievements } = props;
+  // const userId = 1;
+  // const achievementName = "something shiny";
 
-  const data = {
-    userId: 1, // Replace with the actual userID
-  };
+  // const data = {
+  //   userId: 1, // Replace with the actual userID
+  // };
 
-  fetch("/api/updateAchievements", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to update achievement");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Achievement updated successfully:", data.achievement);
-    })
-    .catch((error) => {
-      console.error("Failed to update achievement:", error);
-    });
+  // fetch("/api/updateAchievements", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(data),
+  // })
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       throw new Error("Failed to update achievement");
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log("Achievement updated successfully:", data.achievement);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Failed to update achievement:", error);
+  //   });
 
   return (
     <div className="dashboard">
@@ -39,9 +39,10 @@ export default function Dashboard(props) {
         isMusicPlaying={isMusicPlaying}
         handleMusicToggle={handleMusicToggle}
         windowTitle="POKÉDEX"
+        db_achievements={db_achievements}
       />
       <div className="dash-body">
-        <CharSection />
+        <CharSection db_moves={db_moves}/>
         <EnterGame setMode={setMode} />
       </div>
     </div>
