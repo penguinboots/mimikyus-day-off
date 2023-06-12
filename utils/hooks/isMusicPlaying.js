868271d8af85e90f02b4ceaa956640319f978a42
penguinboots <sabrina.ynw@gmail.com>
2023-06-11 21:08:37 -0700

    remove redundancy

--- utils/hooks/isMusicPlaying.js
+++ utils/hooks/isMusicPlaying.js
@@ -2,24 +2,22 @@ import { useState, useRef, useEffect } from 'react';
 
 export default function useIsMusicPlaying(audioRef, mode) {
   const [isMusicPlaying, setMusicPlaying] = useState(false);
-  const originalVolumeRef = useRef(0.05);
+  const originalVolumeRef = useRef(0.1);
 
   const handleMusicToggle = () => {
     setMusicPlaying(prev => !prev);
-    audioRef.current.volume = isMusicPlaying ? originalVolumeRef.current : 0.05;
+    audioRef.current.volume = isMusicPlaying ? originalVolumeRef.current : 0.1;
   };
 
   useEffect(() => {
-    if (isMusicPlaying && mode === 'DASH' && audioRef.current) {
+    if (isMusicPlaying && audioRef.current) {
       audioRef.current.volume = originalVolumeRef.current;
       audioRef.current.play();
-    } else if (isMusicPlaying && mode === 'PLAY' && audioRef.current) {
-      audioRef.current.volume = originalVolumeRef.current;
-      audioRef.current.play();
-    } else {
+    } 
+    else {
       if (audioRef.current) audioRef.current.pause();
     }
-  }, [isMusicPlaying, mode]);
+  }, [isMusicPlaying, mode, audioRef]);
 
   useEffect(() => {
     return () => {
