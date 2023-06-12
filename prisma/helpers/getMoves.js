595f7adc3c02b4a111165af803b338e847f41b08
DASitby <davidsooley551@gmail.com>
2023-06-12 12:12:27 -0230

    write getMoves helper

---
+++ prisma/helpers/getMoves.js
@@ -0,0 +1,20 @@
+export const getMoves = async (user) => {
+  try {
+      //Get request to refresh User Data
+      const getMovesDataResponse = await fetch(`/api/move?auth0Sub=auth0sub123`, {
+        method: 'GET',
+      });
+  
+      if (getMovesDataResponse.ok) {
+        //return data
+        const movesData = await getMovesDataResponse.json();
+        return movesData;
+      } else {
+        // Handle failure
+        console.error('Failed to fetch moves data');
+      }
+      // Handle error
+    } catch (error) {
+      console.error('Failed to fetch moves data:', error);
+    }
+}
