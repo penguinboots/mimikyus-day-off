b8bf73eb2ffa41966e18dc3be1e04978724c52a1
DASitby <davidsooley551@gmail.com>
2023-06-12 14:00:31 -0230

    import useUser, make test button for createUser, switch db_user calls

--- components/play/TreasureRoom.jsx
+++ components/play/TreasureRoom.jsx
@@ -1,19 +1,26 @@
 import { learnMove } from "@/prisma/helpers/learnMove";
 import { earnAchievement } from "@/prisma/helpers/earnAchievement"
+import { createUser } from "@/prisma/helpers/createUser";
+import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Room(props) {
-  const { db_user } = props
+  const { user, error, isLoading} = useUser()
   return (
     <div className="treasure-room">
       <div className="treasure-floor">
-        <button onClick={()=> {earnAchievement(db_user, "crimes against munchlax")}}>
+        
+      <button onClick={()=> {createUser(user)}}>
+          Click to create user based on auth0 login
+        </button>
+        <br></br>
+        <button onClick={()=> {earnAchievement(user, "crimes against munchlax")}}>
           Click to Update Achievement
         </button>
         <br></br>
-        <button onClick={()=> {learnMove(db_user, 'play-rough')}}>
+        <button onClick={()=> {learnMove(user, 'play-rough')}}>
           Click to Learn Play Rough
         </button>
         <br></br>
-        <button onClick={()=> {learnMove(db_user, 'shadow-sneak')}}>
+        <button onClick={()=> {learnMove(user, 'shadow-sneak')}}>
           Click to Learn Shadow Sneak
         </button>
         <br></br>
