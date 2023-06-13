69fb5544ba27f6597da4910fc89e9b6dfe364e3b
DASitby <davidsooley551@gmail.com>
2023-06-13 16:23:57 -0230

    add button for gettin a berry

--- components/play/TreasureRoom.jsx
+++ components/play/TreasureRoom.jsx
@@ -2,12 +2,15 @@ import { learnMove } from "@/prisma/helpers/learnMove";
 import { earnAchievement } from "@/prisma/helpers/earnAchievement"
 import { createUser } from "@/prisma/helpers/createUser";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { earnItem } from "@/prisma/helpers/earnItem";
 export default function Room(props) {
   const { user, error, isLoading} = useUser()
   return (
     <div className="treasure-room">
       <div className="treasure-floor">
-        
+      <button onClick={()=> {earnItem(user, "berry", 1)}}>
+          Click to get a berry
+        </button>
       <button onClick={()=> {createUser(user)}}>
           Click to create user based on auth0 login
         </button>
