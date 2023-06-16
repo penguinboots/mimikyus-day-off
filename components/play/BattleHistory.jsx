11da8b09758222414b7299c04d6a78f5e32906af
penguinboots <sabrina.ynw@gmail.com>
2023-06-15 20:49:55 -0700

    style battlehistory

--- components/play/BattleHistory.jsx
+++ components/play/BattleHistory.jsx
@@ -1,19 +1,22 @@
 import { useGameState } from "@/utils/context/GameStateContext";
 
-export default function BattleHistory(props) {
+export default function BattleHistory() {
   const { battleHistory, setBattleHistory } = useGameState();
   const currentHistory = battleHistory.slice(-4);
-  console.log(currentHistory);
 
-  const displayHistory = currentHistory.map((historyItem, index) => (
-    <div key={index} className="battle-history-item">
-      {historyItem}
-    </div>
-  ));
+  const displayHistory = currentHistory.map((historyItem, index) => {
+    const darknessLevel = 200 - (index + 1) * 40; // Calculate the darkness level for each item
 
-  return (
-    <div className="battle-history-display">
-      {displayHistory}
-    </div>
-  );
+    const itemStyle = {
+      color: `rgb(${darknessLevel}, ${darknessLevel}, ${darknessLevel})`,
+    };
+
+    return (
+      <div key={index} className="battle-history-item" style={itemStyle}>
+        {historyItem}
+      </div>
+    );
+  });
+
+  return <div className="battle-history-container">{displayHistory}</div>;
 }
