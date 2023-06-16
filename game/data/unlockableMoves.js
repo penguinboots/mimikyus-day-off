3bfb1ac38d073054eba4d2d3157e2266858d93d2
DASitby <davidsooley551@gmail.com>
2023-06-16 11:28:53 -0230

    add final list of unlockable moves for mvp

--- game/data/unlockableMoves.js
+++ game/data/unlockableMoves.js
@@ -9,6 +9,16 @@ const unlockables = [
  {"name":"draining-kiss", "collected": false},
  {"name":"shadow-ball", "collected": false},
  {"name":"drain-punch", "collected": false},
+ {"name":"giga-drain", "collected": false},
+ {"name":"dark-pulse", "collected": false},
+ {"name":"wood-hammer", "collected": false},
+ {"name":"dazzling-gleam", "collected": false},
+ {"name":"thunderbolt", "collected": false},
+ {"name":"leech-life", "collected": false},
+ {"name":"charge-beam", "collected": false},
+ {"name":"psychic", "collected": false},
+ {"name":"screech", "collected": false},
+ {"name":"trailblaze", "collected": false},
 ];
 
 module.exports = { unlockables }
