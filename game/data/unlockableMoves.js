ef2f2f10c0b0f9222285d2dcd6d23c1e0017a705
DASitby <davidsooley551@gmail.com>
2023-06-16 11:34:22 -0230

    order unlockable moves by planned store location

--- game/data/unlockableMoves.js
+++ game/data/unlockableMoves.js
@@ -1,24 +1,27 @@
 const unlockables = [
  {"name":"astonish", "collected": true},
  {"name":"quick-attack", "collected": true},
- {"name":"play-rough", "collected": false},
+ //First Store
  {"name":"shadow-sneak", "collected": false},
- {"name":"shadow-claw", "collected": false},
- {"name":"swords-dance", "collected": false},
- {"name":"charm", "collected": false},
- {"name":"draining-kiss", "collected": false},
  {"name":"shadow-ball", "collected": false},
- {"name":"drain-punch", "collected": false},
+ {"name":"draining-kiss", "collected": false},
+ {"name":"charge-beam", "collected": false},
  {"name":"giga-drain", "collected": false},
  {"name":"dark-pulse", "collected": false},
- {"name":"wood-hammer", "collected": false},
- {"name":"dazzling-gleam", "collected": false},
- {"name":"thunderbolt", "collected": false},
+ //Second Store
  {"name":"leech-life", "collected": false},
- {"name":"charge-beam", "collected": false},
- {"name":"psychic", "collected": false},
- {"name":"screech", "collected": false},
+ {"name":"charm", "collected": false},
  {"name":"trailblaze", "collected": false},
+ {"name":"wood-hammer", "collected": false},
+ {"name":"thunderbolt", "collected": false},
+ {"name":"psychic", "collected": false},
+ //Third Store
+ {"name":"swords-dance", "collected": false},
+ {"name":"screech", "collected": false},
+ {"name":"drain-punch", "collected": false},
+ {"name":"dazzling-gleam", "collected": false},
+ {"name":"shadow-claw", "collected": false},
+ {"name":"play-rough", "collected": false},
 ];
 
 module.exports = { unlockables }
