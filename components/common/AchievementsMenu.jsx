4da1f9faed71d56f92ad14e8c6a90fae6920c5dc
penguinboots <sabrina.ynw@gmail.com>
2023-06-15 20:11:46 -0700

    style achievements menu

--- components/common/AchievementsMenu.jsx
+++ components/common/AchievementsMenu.jsx
@@ -1,9 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faXmark, faFan } from "@fortawesome/free-solid-svg-icons";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useState, useEffect } from "react";
 import Achievement from "./Achievement";
 import { getAchievements } from "@/prisma/helpers/getAchievements";
+import localFont from "next/font/local";
+
+const vt = localFont({ src: "../../public/fonts/VT323-Regular.ttf" });
 const achievementList = require("../../game/data/achievements.json");
 
 export default function AchievementsMenu(props) {
@@ -15,10 +18,10 @@ export default function AchievementsMenu(props) {
     const fetchData = async () => {
       try {
         const db_data = await getAchievements(user);
-        const db_achievements = db_data.achievements
+        const db_achievements = db_data.achievements;
         setplayerEarnedAchievements(db_achievements);
       } catch (error) {
-        console.error('Error fetching achievements:', error);
+        console.error("Error fetching achievements:", error);
       }
     };
 
@@ -43,8 +46,28 @@ export default function AchievementsMenu(props) {
       <div className="close-window" onClick={props.handleClick}>
         <FontAwesomeIcon icon={faXmark} />
       </div>
-      <h2>Achievements</h2>
-      <ul className="ach-list">{achievements}</ul>
+      <h2
+        style={{
+          fontFamily: vt.style.fontFamily,
+        }}
+      >
+        ACHIEVEMENTS
+      </h2>
+      {achievements.length > 0 ? (
+        <ul className="ach-list">{achievements}</ul>
+      ) : (
+        <div className="spinner-container">
+          <FontAwesomeIcon className="spinner" icon={faFan} />
+          <h4
+            style={{
+              fontFamily: vt.style.fontFamily,
+              fontSize: "16px",
+            }}
+          >
+            LOADING...
+          </h4>
+        </div>
+      )}
     </div>
   );
 }
