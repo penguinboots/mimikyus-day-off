7203f6eff54437c822bc1bc4480ee556f58d3c57
penguinboots <sabrina.ynw@gmail.com>
2023-06-16 20:32:36 -0700

    move spinner into store component

--- components/play/StoreOption.jsx
+++ components/play/StoreOption.jsx
@@ -1,7 +1,7 @@
 export default function StoreOption(props) {
   const { name, chosenOption, handleChooseOption } = props;
   return (
-    <div className={`option ${chosenOption === name ? "chosen" : ""}`}>
+    <div className={`option ${chosenOption.item === name ? "chosen" : ""}`}>
       <div>{name}</div>
       <button onClick={() => handleChooseOption(name)}>CHOOSE</button>
     </div>
