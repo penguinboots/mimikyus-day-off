0ebabf2fb9a567582d327783473591aff3846f98
penguinboots <sabrina.ynw@gmail.com>
2023-06-18 14:10:39 -0700

    build end-game-room

--- components/play/EndGameRoom.jsx
+++ components/play/EndGameRoom.jsx
@@ -1,3 +1,5 @@
+import { useGameState } from "@/utils/context/GameStateContext";
+
 export default function EndGameRoom() {
   const { gameState, setGameState, winGame } = useGameState();
   const BACKGROUND = gameState.currentRoom.background;
@@ -5,14 +7,16 @@ export default function EndGameRoom() {
 
   return (
     <div
-      className="endGameRoom"
+      className="end-game-room"
       style={{
         backgroundImage: BACKGROUND,
         backgroundColor: BACKGROUND_COL,
         position: "relative",
       }}
     >
-      <div></div>
+      <div className="end-game-container">
+        {/* YOUR CODE HERE ! */}
+      </div>
     </div>
   );
 }
