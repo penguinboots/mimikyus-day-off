ab4bdabc4fffb1473a78d154658a7783157dd600
DASitby <davidsooley551@gmail.com>
2023-06-19 18:06:45 -0230

    remove unnecessary div

--- components/play/AchievementPopup.jsx
+++ components/play/AchievementPopup.jsx
@@ -1,13 +1,13 @@
 export default function AchievementPopup(props) {
   const { achievement } = props
   return (
-    <div>
+    <>
       {achievement && (
         <>
           <span>ACHIEVEMENT GET!</span>
           <img src={`/achievements/${achievement.name}.png`} alt={achievement.title} />
         </>
       )}
-    </div>
+    </>
   );
 }
